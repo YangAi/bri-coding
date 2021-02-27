3466f32300a28003568bcb145e9b9d985f888a55
YangAi <yangai@iu.edu>
2021-02-27 16:26:28 -0500

    fix host issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8000'
+    baseURL: 'http://localhost:3000'
   },
 
   /*
