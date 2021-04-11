62baa3e7ea6f23c598952feda9d7b4db9e35ec3c
YangAi <yangai@iu.edu>
2021-04-11 03:29:06 -0400

    save

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://bri-coding.herokuapp.com/api'
+    baseURL: '/api'
   },
 
   /*
